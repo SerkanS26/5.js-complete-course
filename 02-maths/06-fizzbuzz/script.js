78c9881e6b4d9a0e8181348985ec178eb246260f
serkan <s.safran84@gmail.com>
2021-01-20 11:09:45 +0100

    if statement modified

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -15,7 +15,7 @@
             console.log("FizzBuzz");
         }else if(i % 3 === 0){
             console.log("Fizz");
-        }if (i % 5 === 0){
+        }else if(i % 5 === 0){
             console.log("Buzz");
         }else{
             console.log(i);
